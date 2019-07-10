ccdfc6f3a8e8d72fb2c0bfac17ec24cd31fafeff
Anjaneyulu Korukonda <kk_anji@rediffmail.com>
2019-07-11 00:05:05 +0530

    Updated line4 to mimic merge conflict

--- Build_Legos.sh
+++ Build_Legos.sh
@@ -1,6 +1,6 @@
 This is a test line 1 changed in master
 Second line added in master branch
 This is line 2 from sprint1
-Build_Legos.sh version 1.0
+Build_Legos.sh version 1.6
 Build_Legos.sh version 1.1
 Build_Legos.sh version 1.2
